66398322176f037faf589b041758668765db4679
VODOLEY <olhovcki.a.c@gmail.com>
2022-06-25 19:08:22 +0300

    Add task 3

--- lesson_1.py
+++ lesson_1.py
@@ -1,7 +1,7 @@
 # Задание 2
 
-add_mi_sec = int(input('Введите время в секундах - '))
-mi_sec = add_mi_sec % 60
-mi_minute = add_mi_sec % 3600 // 60
-mi_hour = add_mi_sec // 3600
-print('%02d'% mi_hour, ':', '%02d'% mi_minute, ':', '%02d'% mi_sec)
+n = input('Введите число - ')
+a = int(n)
+b = int (n + n)
+c = int(n + n + n)
+print(a + b + c)
