8b0ddc8fc55364695da117560b5c6fc3f6c13ddf
VODOLEY <olhovcki.a.c@gmail.com>
2022-06-25 19:31:53 +0300

    improvements

--- lesson_1.py
+++ lesson_1.py
@@ -4,4 +4,4 @@ add_mi_sec = int(input('Введите время в секундах - '))
 mi_sec = add_mi_sec % 60
 mi_minute = add_mi_sec % 3600 // 60
 mi_hour = add_mi_sec // 3600
-print('%02d'% mi_hour, ':', '%02d'% mi_minute, ':', '%02d'% mi_sec)
+print('%02d:%02d:%02d'%(mi_hour, mi_minute, mi_sec))
