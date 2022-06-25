a7c0585e60ce0afdb4c92f8ec89befb8b1b2022d
VODOLEY <olhovcki.a.c@gmail.com>
2022-06-25 18:11:59 +0300

    Сhanges task 2

--- lesson_1.py
+++ lesson_1.py
@@ -1,4 +1,7 @@
 # Задание 2
 
-add_sec = input('Введите число в секундах - ')
-print(add_sec)
+add_mi_sec = int(input('Введите время в секундах - '))
+mi_sec = add_mi_sec % 60
+mi_minute = add_mi_sec % 3600 // 60
+mi_hour = add_mi_sec // 3600
+print('%02d'% mi_hour, ':', '%02d'% mi_minute, ':', '%02d'% mi_sec)
