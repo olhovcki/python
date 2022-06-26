1373e03da126ec17bddb559337e4b509846db519
VODOLEY <olhovcki.a.c@gmail.com>
2022-06-26 14:35:23 +0300

    Add task 6

--- lesson_1.py
+++ lesson_1.py
@@ -1,9 +1,14 @@
-# Задание 5
+# Задание 6
 
 mi_revenue = int(input('Введите значение выручки - '))
 mi_costs = int(input('Введите значение издержек - '))
 
 if mi_revenue > mi_costs:
-    print('Ваша фирма является прибыльной!')
+    mi_profit = mi_revenue - mi_costs
+    mi_profitability_revenue = mi_profit / mi_revenue * 100
+    print('Ваша фирма является прибыльной!','\nРентабельность составит -','%.2f'%mi_profitability_revenue,'%')
+    num_employees = int(input('Сколько у Вас сотрудников? - '))
+    mi_profit_employees = mi_profit / num_employees
+    print('Прибыль на одного сотрудника составит -','%.2f'%mi_profit_employees, 'тубриков')
 else:
     print('Ваша фирма является убыточной!')
